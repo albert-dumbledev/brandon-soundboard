e0978f122e59840febf92b0aabf92f49a9fffcea
Albert Wang <albertwang2002@hotmail.com>
2016-04-30 18:18:43 +1000

    Initial commit

---
+++ public/soundboard.js
@@ -0,0 +1,54 @@
+$("#pls-no").click(function () {
+	document.getElementById("pls-no-embed").innerHTML="<embed src='pls_no.mp3' autostart=true loop=false volume=100 hidden=true>";
+   	return true;
+});
+
+$("#fuck-you").click(function () {
+	document.getElementById("fuck-you-embed").innerHTML="<embed src='fuck_you.mp3' autostart=true loop=false volume=100 hidden=true>";
+   	return true;
+});
+
+$("#drop-you-legit").click(function () {
+	document.getElementById("drop-you-legit-embed").innerHTML="<embed src='drop_legit.mp3' autostart=true loop=false volume=100 hidden=true>";
+   	return true;
+});
+
+$("#heavy-breathing").click(function () {
+	document.getElementById("heavy-breathing-embed").innerHTML="<embed src='heavy_breathing.mp3' autostart=true loop=false volume=100 hidden=true>";
+   	return true;
+});
+
+$("#dat-disrespect").click(function () {
+	document.getElementById("dat-disrespect-embed").innerHTML="<embed src='dat_disrespect.mp3' autostart=true loop=false volume=100 hidden=true>";
+   	return true;
+});
+
+$("#disrespect-me").click(function () {
+	document.getElementById("disrespect-me-embed").innerHTML="<embed src='disrespect_me.mp3' autostart=true loop=false volume=100 hidden=true>";
+   	return true;
+});
+
+$("#no-nooo").click(function () {
+	document.getElementById("no-nooo").innerHTML="<embed src='no_nooo.mp3' autostart=true loop=false volume=100 hidden=true>";
+   	return true;
+});
+
+$("#u-ok").click(function () {
+	document.getElementById("u-ok-embed").innerHTML="<embed src='u_ok.mp3' autostart=true loop=false volume=100 hidden=true>";
+   	return true;
+});
+
+$("#do-anything").click(function () {
+	document.getElementById("do-anything-embed").innerHTML="<embed src='did_not_even_do_anything.mp3' autostart=true loop=false volume=100 hidden=true>";
+   	return true;
+});
+
+$("#outplay").click(function () {
+	document.getElementById("outplay-embed").innerHTML="<embed src='1v1_outplay.mp3' autostart=true loop=false volume=100 hidden=true>";
+   	return true;
+});
+
+$("#hello").click(function () {
+	document.getElementById("hello-embed").innerHTML="<embed src='hello.mp3' autostart=true loop=false volume=100 hidden=true>";
+   	return true;
+});
