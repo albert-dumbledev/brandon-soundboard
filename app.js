1fbeb2235a3814ace659272200a31834309b09da
Albert Wang <albertwang2002@hotmail.com>
2016-07-01 02:25:48 +1000

    Generate some sick ass fresh beats

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.sendFile( __dirname + "/view/index.html");
+	var brandonFiles = fs.readdirSync(__dirname + "/public/brandon");
+	var luiFiles = fs.readdirSync(__dirname + "/public/lui");
+
+	var brandonSoundFiles = brandonFiles.filter(function (file) {
+		return file.indexOf("mp3") != -1;
+	});
+
+	var luiSoundFiles = luiFiles.filter(function (file) {
+		return file.indexOf("mp3") != -1;
+	});
+
+	console.log(brandonSoundFiles);
+	console.log(luiSoundFiles);
+	res.render(__dirname + "/view/index", {
+		brandon: brandonSoundFiles,
+		lui: luiSoundFiles
+	});
 });
 
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+	console.log('Example app listening on port 3000!');
 });
--- public/soundboard.js
+++ public/soundboard.js
@@ -1,54 +1,43 @@
-$("#pls-no").click(function () {
-	document.getElementById("pls-no-mp3").play();
-   	return true;
+var sickBeat = document.getElementById("sick-beat-mp3");
+
+function playSound(id) {
+	document.getElementById(id + "-mp3").play();
+}
+
+$(".sound-bite-btn").click(function (e) {
+	playSound($(e.target).attr("id"));
+	return true;
 });
 
-$("#fuck-you").click(function () {
-	document.getElementById("fuck-you-mp3").play();
-   	return true;
+// MIX SUM FKEN SICK ASS BEATS YO
+$(".start-sick-jam").click(function (e) {
+	sickBeat.play();
+	$(".sound-bite").each(function (index) {
+		// MUSICAL GENIUS
+		var randomInterval = Math.floor(Math.random() * (36 - 12)) + 12;
+		var that = this;
+
+		setTimeout(function () {
+			setInterval(function () {
+				if ($(that).attr("id") !== "fuck-you-mp3") {
+					that.play();	
+				}
+			}, randomInterval*1000);	
+		}, index*1000);
+	});
 });
 
-$("#drop-you-legit").click(function () {
-	document.getElementById("drop-you-legit-mp3").play();
-   	return true;
+$(".stop-sick-jam").click(function (e) {
+	sickBeat.pause();
+	sickBeat.currentTime = 0;
+	//Lazy hack
+	for (var i = 1; i < 99999; i++) {
+        clearInterval(i);
+	}
 });
 
-$("#heavy-breathing").click(function () {
-	document.getElementById("heavy-breathing-mp3").play();
-   	return true;
-});
-
-$("#dat-disrespect").click(function () {
-	document.getElementById("dat-disrespect-mp3").play();
-   	return true;
-});
-
-$("#disrespect-me").click(function () {
-	document.getElementById("disrespect-me-mp3").play();
-   	return true;
-});
-
-$("#no-nooo").click(function () {
-	document.getElementById("no-nooo-mp3").play();
-   	return true;
-});
-
-$("#u-ok").click(function () {
-	document.getElementById("u-ok-mp3").play();
-   	return true;
-});
-
-$("#do-anything").click(function () {
-	document.getElementById("do-anything-mp3").play();
-   	return true;
-});
-
-$("#outplay").click(function () {
-	document.getElementById("outplay-mp3").play();
-   	return true;
-});
-
-$("#hello").click(function () {
-	document.getElementById("hello-mp3").play();
-   	return true;
-});
+// LOOP UNTIL U DIE
+sickBeat.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
